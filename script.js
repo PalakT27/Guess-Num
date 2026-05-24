807d715b6f67a49308ce88552733df93510b4a1d
PalakT27 <palakt058@gmail.com>
2026-05-24 15:34:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let CorrectNum = 20;
+let input = document.querySelector("input");
+let CheckBtn = document.querySelector("#CheckBtn");
+let msg = document.querySelector("#msg");
+
+CheckBtn.onclick = () => {
+    let UserNum = Number(input.value);
+    if (UserNum === CorrectNum){
+        msg.innerText = "Congratulation 🎉... You entered the correct Number";
+        msg.style.color = "orange";
+    }
+    else if (UserNum > CorrectNum){
+        msg.innerText = "Too high 📈... Guess Again.";
+        msg.style.color = "red";
+    }
+    else
+    {
+        msg.innerText = "Too low 📉... Guess Again.";
+        msg.style.color = "green";
+    }
+};
